87bc9e4f415e457dc3ec022283d84d10db42d0f5
anas <muhdanas@uw.edu>
2021-03-18 21:27:53 -0700

    Update tutorial and test

--- tests/testthat/test-my_knn_cv.R
+++ tests/testthat/test-my_knn_cv.R
@@ -4,6 +4,6 @@ test_that("Error check if contain NA in data", {
 
 my_penguins <- my_penguins %>% drop_na()
 
-test_that("Output check for type of data", {
+test_that("Output type of data check", {
   expect_type(my_knn_cv(my_penguins, my_penguins[["species"]], 1, 2), "list")
 })
